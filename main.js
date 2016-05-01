39fa9bfd8cbec35419d50cecb0775365766bd763
Cheney Lin <lincheney@gmail.com>
2016-05-01 11:47:52 +1000

    add files

---
+++ main.js
@@ -0,0 +1,206 @@
+function duration_to_string(seconds) {
+    var minutes = Math.round(seconds / 60);
+    seconds = seconds % 60;
+    return minutes + ':' + ('0' + seconds).slice(-2);
+}
+
+function datetime_to_string(date) {
+    date = new Date(date);
+    return (date - (new Date(0))) != 0 && date.toDateString();
+}
+
+function parse_query(query) {
+    var tokens = query.split(/\s+/);
+    data = {query: []};
+
+    for(var i = 0; i < tokens.length; i ++) {
+        if(tokens[i].startsWith('minDuration=')) {
+            data['minDuration'] = tokens[i].split('=', 2)[1]*1;
+        } else if(tokens[i].startsWith('published=')) {
+            var date = new Date(tokens[i].split('=', 2)[1]);
+            // recreate date to match with locale
+            date = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+            data['published'] = date;
+        } else {
+            data.query.push(tokens[i]);
+        }
+    }
+
+    data.query = data.query.join(' ');
+    return data;
+}
+
+function build_query(query) {
+    var data = {m: '1', range: '1-1000000', form: 'json'};
+    data.fields = ['title', 'id', 'media$content', 'media$expirationDate', 'pubDate', 'description', 'defaultThumbnailUrl'].join(',');
+
+    if (query.published) {
+        data['byPubDate'] = query.published.getTime() + '~' + (query.published.getTime() + 24*3600*1000);
+    }
+
+    if (query.query == '') {
+        data.url = 'http://www.sbs.com.au/api/video_feed/f/Bgtm9B/sbs-section-sbstv';
+        data.parser = JSON.parse;
+    } else {
+        // use /suggest endpoint when query is given so we get sorted results
+        data.url = 'http://www.sbs.com.au/api/video_search/suggest/';
+        data.q = query.query;
+        data.parser = function(response) { return JSON.parse(JSON.parse(response)); };
+    }
+    return data;
+}
+
+function search_videos(query, callback) {
+    var parsed_query = parse_query(query);
+    query = build_query(parsed_query);
+    var url = query.url;
+    var parser = query.parser;
+
+    delete query.url;
+    delete query.parser;
+
+    $.get({
+        url: url,
+        data: query,
+        dataType: 'text',
+        error: function(xhr, status, error) {
+            callback({error: 'Failed to connect to API'});
+        },
+        success: function(data, status, xhr) {
+            try {
+                data = parser(data);
+            } catch(err) {
+                callback({error: 'Malformed data: ' + err});
+            }
+            callback({videos: data.entries}, parsed_query);
+        }
+    })
+}
+
+function process_video_data(data, query) {
+    var videos = (data.videos || []);
+    data.videos = [];
+
+    for(var i = 0; i < videos.length; i ++) {
+        var video = videos[i];
+
+        video['_id'] = /\d+$/.exec(video['id'])[0];
+        video['thumbnail'] = video['plmedia$defaultThumbnailUrl']
+
+        video['duration'] = duration_to_string(video['media$content'][0]['plfile$duration']);
+        video['published'] = datetime_to_string(video['pubDate']);
+        video['expiry'] = datetime_to_string(video['media$expirationDate']);
+
+        if (query.minDuration && query.minDuration > video['media$content'][0]['plfile$duration']) {
+            continue;
+        }
+        if (query.published && datetime_to_string(query.published) != video.published) {
+            continue;
+        }
+
+        var expiry = new Date(video['media$expirationDate']);
+        if((expiry - (new Date(0))) == 0) {
+            expiry = null;
+        }
+        // expires within 2 days
+        video['expires_soon'] = (expiry && expiry < (Date.now() + 3600*1000*24*2));
+
+        data['videos'].push(video);
+    }
+
+    return data;
+}
+
+function process_link_data(data) {
+    var links = (data.links || []);
+
+    links = _.unique(links, 'url');
+    for(var i = 0; i < links.length; i ++) {
+        if (!links[i].bitrate) {
+            links[i].bitrate = bitrate_from_url(links[i].url);
+        }
+
+        // set a link name, using bitrate or file extension (or 'Link')
+        if (links[i].bitrate) {
+            links[i].name = links[i].bitrate + 'K';
+        } else {
+            var extension = /\.([^./?]+)(\?|$)/.exec(links[i].url);
+            if (extension) {
+                links[i].name = extension[1].toUpperCase();
+            } else {
+                links[i].name = 'Link';
+            }
+        }
+    }
+
+    links.sort(function(a, b) {
+        return (b.bitrate || 0) - (a.bitrate || 0);
+    });
+
+    // group all the links by type
+    links = _.pairs(_.groupBy(links, 'type'));
+    for(var i = 0; i < links.length; i ++) {
+        links[i] = {type: links[i][0], urls: links[i][1]};
+    }
+    links = _.sortBy(links, 'type');
+
+    data.links = links;
+    return data;
+}
+
+function set_loading(context, loading) {
+    context.find('.input-control').prop('disabled', loading);
+    context.find('.non-spinner').toggle(!loading);
+    context.find('.spinner').toggle(loading);
+}
+
+function load_video_data(template) {
+    var query = $('#search-box').val();
+    if (query == '') {
+        return;
+    }
+
+    set_loading($('#search-form'), true);
+    $('#video-results').html('');
+
+    search_videos(query, function(result, ...args) {
+        result = process_video_data(result, ...args);
+        result = template.render(result);
+
+        set_loading($('#search-form'), false);
+        $('#video-results').html(result);
+    });
+}
+
+function load_link_data(template, id) {
+    set_loading($('#video-' + id + ' .link-section'), true);
+    $('#video-' + id + ' .link-item').html('');
+
+    fetch_video_links(id, function(result) {
+        result = process_link_data(result)
+        result = template.render(result);
+
+        set_loading($('#video-' + id + ' .link-section'), false);
+        $('#video-' + id + ' .link-item').html(result);
+    });
+}
+
+$(document).ready(function() {
+
+    // compile template
+    var video_template = Hogan.compile($('#video-template').html());
+    var link_template = Hogan.compile($('#link-template').html());
+
+    $('#search-form').on('submit', function(e) {
+        load_video_data(video_template);
+        e.preventDefault();
+    });
+
+    $('body').on('click', 'button.link-fetcher', function() {
+        var id = this.getAttribute('data-video-id');
+        load_link_data(link_template, id);
+    });
+
+    $('body').on('click', '.video-link-box', function(){ this.select(); });
+
+})
---
+++ video-fetcher.js
@@ -0,0 +1,150 @@
+function bitrate_from_url(url) {
+    // eg http://sbsauvod-f.akamaihd.net/SBS_Production/managed/2014/09/04/2014-09-04_273396_1500K.mp4?...
+    // eg http://videocdn.sbs.com.au/u/video/SBS_Production/managed/2015/10/30/555273795946_1500K.mp4
+    // eg rtmp://cp99272.edgefcs.net/ondemand/pub/DOC_EXT_No_Turning_Back_Tha_507_549627_1500K.mp4
+    // eg http://sbsvodns-vh.akamaihd.net/z/vod/SBS_Production/managed/2016/04/11/2016-04-11_452901_,1500,K.mp4.csmil/manifest.f4m?...
+    var match = /_,?(\d+),?K\.mp4/.exec(url);
+    return (match && match[1]*1);
+}
+
+function fetch_video_links(id, callback) {
+    var url = 'http://www.sbs.com.au/api/video_pdkvars/id/' + id;
+
+    $.get({
+        url: url,
+        dataType: 'text',
+        error: function(xhr, status, error) {
+            callback({error: 'Error loading ' + url});
+        },
+        success: function(data, status, xhr) {
+            var smils = [];
+            data = data.split('&');
+            // grab all release urls
+            for(var i = 0; i < data.length; i ++) {
+                var kv = data[i].split('=', 2);
+                if (decodeURIComponent(kv[0]).startsWith('releaseUrls') && kv[1]) {
+                    smils.push('http:' + decodeURIComponent(kv[1]));
+                }
+            }
+
+            fetch_smils(smils, [], function(links) {
+                callback({links: links});
+            });
+        },
+    });
+}
+
+function fetch_smils(smils, links, callback) {
+    if (smils.length == 0) {
+        callback(links);
+        return;
+    }
+
+    var url = smils.pop();
+
+    $.get({
+        url: url,
+        dataType: 'xml',
+        error: function(xhr, status, error) {
+            console.log('Error fetching: ' + url);
+            fetch_smils(smils, links, callback);
+        },
+        success: function(data, status, xhr) {
+            data = $(data);
+
+            // base is used for rtmp links
+            var base = data.find('meta').attr('base');
+            base = base && base.replace(/\/+$/, '');
+
+            data.find('textstream').each(function(i, e) {
+                links.push({type: 'Captions', url: e.getAttribute('src')});
+            });
+
+            var sources = data.find('video').map(function(i, e) {
+                return e.getAttribute('src');
+            }).get();
+            sources = _.unique(sources);
+
+            parse_video_sources(sources, base, links, function(links) {
+                fetch_smils(smils, links, callback);
+            });
+        },
+    });
+}
+
+function parse_video_sources(sources, base, links, callback) {
+    if (sources.length == 0) {
+        callback(links);
+        return;
+    }
+
+    var src = sources.pop();
+
+    if (src.match(/^http(s?):\/\/sbs[a-z]+-vh\.akamaihd\.net/)) {
+        // encrypted
+        if (!src.match('manifest.f4m?')) {
+            // m3u
+            get_urls_from_m3u(src, function(new_links) {
+                parse_video_sources(sources, base, links.concat(new_links), callback);
+            });
+            return;
+        }
+
+        // f4m/hds
+        src += '&hdcore=';
+        if (src.match(/_\d+K\.mp4/)) {
+            links.push({type: 'F4M', url: src});
+        } else {
+            // manifest containing multiple bitrate links
+            // we create a separate link for each bitrate
+            var match = /_,((\d+,)+)K\.mp4\.csmil/.exec(src);
+            var bitrates = match[1].split(',');
+            for(var i = 0; i < bitrates.length; i ++) {
+                if (bitrates[i] != '') {
+                    links.push({type: 'F4M', url: src.replace(match[1], bitrates[i] + ',')});
+                }
+            }
+        }
+
+    } else if (src.match(/^http(s?):\/\/sbs[a-z]+-f\.akamaihd\.net/)) {
+        // unencrypted
+        links.push({type: 'FLV', url: src + '?v=&fp=&r=&g='});
+    } else if (!src.startsWith('https://') && base) {
+        // rtmp
+        links.push({type: 'RTMP', url: base + '/' + src.replace(/^\/+/, '')})
+    } else if (src.startsWith('http://videocdn.sbs.com.au/u/video')) {
+        // direct
+        links.push({type: 'Direct', url: src});
+    } else if (src.match(/^http(s?):\/\/pubads/)) {
+        // ads
+    } else {
+        // unhandled
+        console.log('Unhandled source: ' + src);
+        links.push({type: 'unknown', url: src});
+    }
+
+    parse_video_sources(sources, base, links, callback);
+}
+
+function get_urls_from_m3u(url, callback) {
+    $.get({
+        url: url,
+        dataType: 'text',
+        error: function(xhr, status, error) {
+            console.log('Error fetching: ' + url);
+            callback(links);
+        },
+        success: function(data, status, xhr) {
+            // data is in m3u playlist format
+            data = data.split('\n#');
+            var links = [];
+            // ignore ext m3u header
+            for(var i = 1; i < data.length; i ++) {
+                var lines = data[i].split('\n');
+                var bitrate = /BANDWIDTH=(\d+),/.exec(lines[0])[1] / 1000;
+                links.push({type: 'M3U', url: lines[1], bitrate: Math.round(bitrate)});
+            }
+            callback(links);
+        },
+    });
+}
